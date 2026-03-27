74fdafc8aa6be9642a861b20ad81a79b60149f00
mohd ashab <mohdashab683@gmail.com>
2026-03-28 01:50:03 +0530

    Add files via upload

---
+++ 02_variables/script.js
@@ -0,0 +1,16 @@
+
+  let FirstName = 'mohd'
+ let LastName = 'ashab'
+ let age = 18
+ let isHappy = true
+ let a
+ a = 'mohd'
+
+ const hoursInDay = 24
+ 
+  console.log(FirstName)
+
+
+ let userIntro =  'hi , my name is '
+  + a + ' '  
+  + LastName + '. ' + 'i am' + ' ' + age + ' ' + 'years old'
