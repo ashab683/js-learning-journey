25dc4be7b95faef394319b5fd2b3fd96c310295d
mohd ashab <mohdashab683@gmail.com>
2026-03-28 01:50:21 +0530

    Add files via upload

---
+++ 03_code-execution-in-javascript/script.js
@@ -0,0 +1,10 @@
+debugger
+console.log(firstName)
+var firstName = 'mohd'
+let lastName = 'ashab'
+let age = 18
+const yearOfBirth = 2006
+
+
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
