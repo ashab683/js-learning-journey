9b9cf0f3cbc91806e0a23105fec20b41ba86187b
mohd ashab <mohdashab683@gmail.com>
2026-03-29 19:20:24 +0530

    Add files via upload

---
+++ 05_stings-methods-and-properties/script.js
@@ -0,0 +1,12 @@
+const message = 'hello world'
+const faltuMessage = '    hi , i am ashab    '
+const finalMessage = faltuMessage.trim()
+
+const lastFourdigits = '7860' 
+const maskedAccountNumbe = lastFourdigits.padStart(16,'*' )
+
+const capitalMessage = message.toUpperCase()
+
+const concatenatedString = `last four digit of my account number is `.concat(' ',lastFourdigits) 
+
+const templateString = `my account number is ${lastFourdigits}`
