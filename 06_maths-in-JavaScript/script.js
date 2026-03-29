4b6d31dc97acf47acb27c7b61d22abf8a6b2366a
mohd ashab <mohdashab683@gmail.com>
2026-03-29 19:20:51 +0530

    Add files via upload

---
+++ 06_maths-in-JavaScript/script.js
@@ -0,0 +1,5 @@
+const  width = prompt('please enter rectangle width')
+const height = prompt('please enter rectangle height')
+
+console.log(width * height);
+// document.write(`rectangle area: ${width * height}`)
