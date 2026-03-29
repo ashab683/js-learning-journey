c6486f73f9ed0fa69b4862ec1cf97d5407067a4c
mohd ashab <mohdashab683@gmail.com>
2026-03-29 19:21:34 +0530

    Add files via upload

---
+++ 09_logical-operators/script.js
@@ -0,0 +1,25 @@
+ const userAge = 15
+
+const isSchoolStudent = (userAge >=5) && (userAge <=18 ) 
+const isColledgeStudent = (userAge >=18) && (userAge <=24) 
+
+const isStudent =  isSchoolStudent || isColledgeStudent 
+
+const andResult = 0 && 2
+const orResult = 0 || 2
+
+const andResult3 = 'hello' && 'null'
+const orResult3 = 'hello' || 'null'
+
+const andResult4 = 'hello' && -4 + 4
+const orResult4 = undefined || 4+6*5
+
+const andResult5  = 'hello' && console.log('hello1')
+const orResult5 = undefined || console.log('hello2')
+
+
+
+
+
+
+
