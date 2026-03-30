77ea2957d91c091c92e02a614c6c8b248a02bd46
mohd ashab <mohdashab683@gmail.com>
2026-03-30 22:25:44 +0530

    Add files via upload

---
+++ 14_ternary-operator/script.js
@@ -0,0 +1,70 @@
+
+const  username = 5>2 ? 'ashab' : 'procodrr'
+
+const gender = 'f'
+
+
+const userMessage =  `${gender=='f' ? 'she' : 'he'} is a college student.`
+console.log(userMessage)
+
+
+const result = null ? 'mohd' : 'ashab' ? '18' : 0
+             // 0 =falsy value , 1 = truthy value
+console.log(result);
+
+const goat = 'cr7'
+
+const whoIsGoat= `${goat=='cr7'? 'Cristiano Ronaldo' : 'Messi'}`
+
+
+console.log(whoIsGoat);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
