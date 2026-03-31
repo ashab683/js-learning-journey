d85603194d3a858824e63d53b9ec9c10c2f4040e
mohd ashab <mohdashab683@gmail.com>
2026-03-31 23:54:02 +0530

    Add files via upload

---
+++ 18_array/script.js
@@ -0,0 +1,12 @@
+const username = 'mohd ashab'
+const fruitsCollection = [
+    'Apple',
+     'Banana', 
+     'grapes',
+      'dates',
+      {name:'ashab',as:{age:18}},
+
+
+
+      
+]
---
+++ 19_basic-array-method/script.js
@@ -0,0 +1,14 @@
+const evenNumber = [0,2,4,6,8]
+const animal = ['lion','cat','dog']
+const oddNumber = [1,3,5]
+// evenNumbers.shift()
+// evenNumbers.unshift(0)
+
+evenNumber.concat(animal)
+
+const addedArray1 = evenNumber.concat(animal)
+const addedArray2 = animal.concat(evenNumber,oddNumber)
+
+const elementIndex = animal.indexOf('cat')
+const isInclude = animal.includes('dog') 
+
