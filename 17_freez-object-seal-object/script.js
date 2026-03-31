832d523352dbc4599caa2d076b38e0e5362d89df
mohd ashab <mohdashab683@gmail.com>
2026-03-31 23:53:25 +0530

    Add files via upload

---
+++ 17_freez-object-seal-object/script.js
@@ -0,0 +1,36 @@
+const myName = 'ashab'
+
+const userName1 = ''
+const userName2 = ''
+
+const user1 = {
+    firstName : 'mohd'
+}
+
+const user2 = {
+  firstName :'aman',
+ lastName : 'cristiano',
+address:{
+  city:'kanpur',
+  pincode:"28001",
+  state:'up',
+  moteDetails:{
+    population:32537232,
+    area:'2322 sq km',
+
+  }
+}
+
+}
+// user2.age = 18
+// user2["is-student"] = true
+// const user2 = {
+//   firstName :'aman',
+//  'last-Name' : 'cristiano',
+//  'myName':'ashab',
+//  'ashab': 'developer'
+// }
+// console.log(user2.firstName);
+// console.log(user2["last-Name"]);
+// console.log(user2["myName"]);
+// console.log(user2["first" + "Name"]);
