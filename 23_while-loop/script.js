e6c55eda0dac915919097c52c7433b436b1b90ff
mohd ashab <mohdashab683@gmail.com>
2026-04-01 23:28:02 +0530

    Add files via upload

---
+++ 23_while-loop/script.js
@@ -0,0 +1,22 @@
+console.log('program Started')
+// debugger
+// let num = 0
+
+// while(num<100) {
+//     console.log(num);
+//     // num = num + 1;
+//     num++
+// }
+
+const friendsName = ['imran',
+    'talha',
+    'raheem',
+    'hayat',
+    'shoaib-bhai']
+let i = 0
+while(i<friendsName.length){
+    console.log(`${i + 1}.${friendsName[i]} ${'procodrr'}`);
+    i++
+}b
+
+console.log('program ended');
