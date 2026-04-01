7c6300247d81f92e812b79e11c250e838fbee85f
mohd ashab <mohdashab683@gmail.com>
2026-04-01 23:29:05 +0530

    Add files via upload

---
+++ 25_Function/script.js
@@ -0,0 +1,24 @@
+
+//function Defination //(username)=parameter
+function introduceMe(username,profession,age) {    
+   
+    console.log('hi,');
+console.log(`my name is ${username||'procodrr'}`);
+console.log(`i am a ${profession}`);
+console.log(`he is ${age} year old`);
+
+
+}
+//                 function call
+// const returnvalue = introduceMe()
+
+introduceMe('mohd','software engineer',19)       
+introduceMe('ashab','Web developer',18)
+
+
+
+
+//('ashab')= argument
+
+
+
