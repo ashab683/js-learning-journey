844b90b6f0330933e41296e3025d8ae4a974ea8b
mohd ashab <mohdashab683@gmail.com>
2026-04-05 01:45:05 +0530

    Add files via upload

---
+++ 31_lexical-and -block-scope/script.js
@@ -0,0 +1,37 @@
+
+const username = 'ashab'
+
+let userAge = 18
+var a = 50
+
+function subtract() {
+    debugger
+    const x = 5
+    const y = 8
+    const z = 8
+
+    console.log(x - y);
+    console.log(username);
+    function child(){
+        const childName = 'Golu'
+        console.log(childName);
+        console.log(z);
+
+    if(true) {
+        let num1 = 78
+        console.log(num1);
+     }
+
+
+        function grandChild() {
+            const grandChild = 'Molu'
+            console.log(grandChild);
+        }
+        grandChild()
+    }
+    child()
+}
+subtract()
+
+
+console.log('Program ended');
