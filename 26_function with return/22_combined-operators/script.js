99e484e7d01c0a11020ca48b161ceba1b2c2b29a
mohd ashab <mohdashab683@gmail.com>
2026-04-04 16:05:26 +0530

    Add files via upload

---
+++ 26_function with return/22_combined-operators/script.js
@@ -0,0 +1,14 @@
+ let num = 5
+
+// debugger
+// num = num + 2
+// num = num + 2
+
+// num += 5
+
+// num = num*2
+// num = num*2
+// num*=5
+// const newNum = num++
+
+num++    
---
+++ 26_function with return/script.js
@@ -0,0 +1,11 @@
+function addTwoNumber(a,b) {
+    return a + b
+}
+
+const result = addTwoNumber(5,6)
+
+console.log(result);
+
+
+
+
