a32f9e36973811b1b98749dbe544f354d9f7e049
mohd ashab <mohdashab683@gmail.com>
2026-04-04 16:05:47 +0530

    Add files via upload

---
+++ 27_execution-context-in-javaScropt/script.js
@@ -0,0 +1,22 @@
+debugger
+
+sayHI()
+
+var username = 'ashab'
+var userAge = 18
+
+function sayHI() {
+    const a = 14
+    const b = 12
+
+    add(7,9);
+   
+}
+
+function add(x,y) {
+return x + y
+}
+
+console.log('progam ended');
+
+
