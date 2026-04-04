b821dac39e19aae0a8be1a36daca2f123c47fdba
mohd ashab <mohdashab683@gmail.com>
2026-04-05 01:44:22 +0530

    Add files via upload

---
+++ 28_call-stack/script.js
@@ -0,0 +1,29 @@
+ debugger
+
+// sayHI()
+
+// var username = 'ashab'
+// var userAge = 18
+
+// function sayHI() {
+//     const a = 14
+//     const b = 12
+
+//     add(7,9);
+   
+// }
+
+// function add(x,y) {
+// return x + y
+// }
+
+
+function introduceMe(){
+    console.log('hi, my name is mohd ashab');
+    introduceMe()
+}
+introduceMe()
+
+console.log('progam ended');
+
+
