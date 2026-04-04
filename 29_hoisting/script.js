7732d1633175194129fdd85764b3a21812eb4d18
mohd ashab <mohdashab683@gmail.com>
2026-04-05 01:44:47 +0530

    Add files via upload

---
+++ 29_hoisting/script.js
@@ -0,0 +1,31 @@
+
+console.log(username);
+
+var username = 'Ashab'
+
+hi()
+
+//function Definition
+//function declaration
+function hi() {
+
+    console.log('hello');
+    
+
+}
+// sayHi() 
+//function Definition
+//function expression
+
+const sayHi = function() {      //anonymous function
+
+    console.log('Hiii');
+
+}
+sayHi() 
+
+
+
+
+
+
