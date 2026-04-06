c97f6aa7c59b0496a1038c31b973813a087d1d2c
mohd ashab <mohdashab683@gmail.com>
2026-04-06 11:28:53 +0530

    Add files via upload

---
+++ 33_settimeout-and setintervel/script.js
@@ -0,0 +1,22 @@
+// setTimeout(`console.log("hii") 
+//      console.log("i am  ashab")`,500)
+
+debugger
+
+const time1 =  setTimeout(`console.log("hi-1")`, 1000)
+const time2 = setTimeout(`console.log("hi-2")`)
+const time3 = setTimeout(a, 3000,'isj',99,[1,6])
+
+// const time1 =  setinterval(`console.log("hi-1")`, 1000)
+// const time2 = setinterval(`console.log("hi-2")`)
+// const time3 = setinterval(a, 3000,'isj',99,[1,6])
+
+
+function a() {
+
+    console.log("hello world! ");    
+
+}
+console.log('fgusdhf;dfhsljf;sdjfhkds');
+
+
