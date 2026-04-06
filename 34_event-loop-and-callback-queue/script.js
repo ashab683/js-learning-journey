53297f59187072d8848c779feec43ff94dde72a1
mohd ashab <mohdashab683@gmail.com>
2026-04-06 11:29:17 +0530

    Add files via upload

---
+++ 34_event-loop-and-callback-queue/script.js
@@ -0,0 +1,14 @@
+console.log('hi-1');
+
+function hello() {
+    console.log('hello world');
+}
+
+for(let i = 0;i<=2; i++){
+    console.log(i);
+}
+
+hello()
+setTimeout(hello,0)
+
+console.log('hi-2');
