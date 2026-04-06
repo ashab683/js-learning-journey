27b358614679e14dd101a83a76db151c4580cc04
mohd ashab <mohdashab683@gmail.com>
2026-04-07 01:17:42 +0530

    Add files via upload

---
+++ 42_forEach Array Method in JavaScript/script.js
@@ -0,0 +1,12 @@
+const fruits = ['apple','banana','mango','grapes','dragon fruit']
+
+
+
+fruits.forEach(function(fruit){
+    console.log(fruit);
+})
+
+fruits.forEach((fruit)=>{
+    console.log(fruit);
+})
+
