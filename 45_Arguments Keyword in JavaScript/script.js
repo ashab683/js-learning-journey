a3d1260060f9ae71a0c1f1d0fa8410e667f09b92
mohd ashab <mohdashab683@gmail.com>
2026-04-08 23:57:15 +0530

    Add files via upload

---
+++ 45_Arguments Keyword in JavaScript/script.js
@@ -0,0 +1,17 @@
+  const add1 =  ()=>{
+    let sum = 0
+   for(let i = 0; i<arguments.length;i++) {
+    sum = sum + arguments[i]
+   
+}
+return sum
+   } 
+ 
+ const add = function () {
+    let sum = 0
+   for(let i = 0; i<arguments.length;i++) {
+    sum = sum + arguments[i]
+   
+}
+return sum
+   }
