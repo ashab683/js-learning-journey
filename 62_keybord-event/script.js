8c8136f3c69fc3e5983bda7d8c9f205d5c6ebc37
mohd ashab <mohdashab683@gmail.com>
2026-04-15 23:32:11 +0530

    Add files via upload

---
+++ 62_keybord-event/script.js
@@ -0,0 +1,20 @@
+// const h1 = document.querySelector('h1')
+// const input = document.querySelector('input')
+
+
+window.addEventListener('keypress',(e) =>{
+    console.log('code: ',e.Code);
+        console.log('value: ',e.key);
+
+})
+window.addEventListener('keyup',(e) =>{
+    console.log('code: ',e.Code);
+        console.log('value: ',e.key);
+
+})
+window.addEventListener('keydown',(e) =>{
+    console.log('code: ',e.Code);
+        console.log('value: ',e.key);
+
+})
+
