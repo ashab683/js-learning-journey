a2e173bea468e09c680aaacda691132f9f32a757
mohd ashab <mohdashab683@gmail.com>
2026-04-15 23:32:30 +0530

    Add files via upload

---
+++ 63_mouse-event/script.js
@@ -0,0 +1,71 @@
+const h1 = document.querySelector('h1')
+const addCardButton = document.querySelector('.card')
+const container = document.querySelector('.container')
+
+let count = 0
+
+addCardButton.ddEventListener('click', () => {
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+
+addCardButton.ddEventListener('mousedown', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+addCardButton.ddEventListener('mouseup', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+h1.addEventListener('mouseenter', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+addCardButton.addEventListener('mousemove', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+
+addCardButton.addEventListener('mouseout', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+addCardButton.addEventListener('mouseover', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+
+addCardButton.addEventListener('wheel', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
+addCardButton.addEventListener('touchstart', (e) => {
+    console.log(e);
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+    newCard.innerText = count++
+    container.append(newCard)
+})
