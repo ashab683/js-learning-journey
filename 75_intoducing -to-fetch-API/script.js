d3da40b27fb64c0270902463debbe7f9e560687a
mohd ashab <mohdashab683@gmail.com>
2026-04-22 00:18:46 +0530

    Add files via upload

---
+++ 75_intoducing -to-fetch-API/script.js
@@ -0,0 +1,25 @@
+fetch("https://dummyjson.com/products")
+.then((res)=> res.json())
+.then((Data)=> console.log(Data))
+.catch((err)=>{
+    console.log(err);
+})
+
+fetch('https://dummyjson.com/products/add', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: 'BMW Pencil',
+    category:'Stationary'
+    /* other product data */
+  })
+})
+.then(res => res.json())
+.then(console.log);
+
+fetch("https://dummyjson.com/users")
+.then((res) => res.json())
+.then((Data)=>console.log(Data))
+.catch((err)=>{
+    console.log(err);
+})
