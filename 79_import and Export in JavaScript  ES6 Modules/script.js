63034a63593f851c548dd7b1fb42e171d6686eb2
mohd ashab <mohdashab683@gmail.com>
2026-04-25 00:01:06 +0530

    Add files via upload

---
+++ 79_import and Export in JavaScript  ES6 Modules/script.js
@@ -0,0 +1,17 @@
+// import {usersData,myName as myNames} from "./usersData.js";
+import {myName,getFIrstUser} from "./usersData.js";
+
+import usersData from "./usersData.js";
+
+import productsData from "./productsData.js";
+
+console.log(usersData);
+
+console.log(myName);
+
+console.log(productsData);
+
+console.log(getFIrstUser);
+
+import * as something from './usersData.js'
+console.log(something);
